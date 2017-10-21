9803e54f5a6ce461e48fa27fd2bd7ab982cb279d
Sameer <spuri4096@gmail.com>
2017-10-21 16:40:39 -0500

    stashing but I'm lazy

--- real-cost-amazon.js
+++ real-cost-amazon.js
@@ -1,3 +1,5 @@
+CURRENCY_SYMBOL="\u0394";
+$(document.ready(function() {
 $(".sx-price").each( function(i, obj) {
   if($(obj).find('.sx-dash-formatting').length !== 0) {
 
@@ -9,5 +11,14 @@ $(".sx-price").each( function(i, obj) {
   $(obj).find(".sx-price-whole").each(function(id, whole) { $(whole).text(Math.trunc(prices[id])); });
   $(obj).find(".sx-price-fractional").each( function(id, frac) { $(frac).text(Math.trunc((prices[id] % 1)*100)); });
 });
+$(".sx-price-currency").text(CURRENCY_SYMBOL);
 
+$(".s-price").each(function(i, obj) {
+  price = $(obj).text();
+  alert(price);
+  if (!price.startsWith("$")) { alert('baddie'); return; }
+  price = price.substring(1);
+  $(obj).text(CURRENCY_SYMBOL+price);
+});
+});
 
