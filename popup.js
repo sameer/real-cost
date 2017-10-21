c2631dea585c1dbcc4cae48473760d45731f37a3
mingqianwu98 <smilefish64@gmail.com>
2017-10-21 13:54:39 -0500

    Add JS

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
 /* popup js here */
+$(document).ready(function () {
+    $("input[name$='type']").click(function () {
+        var test = $(this).val();
+
+        $("div.desc").hide();
+        $("#Type" + test).show();
+    });
+});
