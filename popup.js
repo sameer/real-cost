33e8467233eff8cdd19e76cd28d9f0749085f644
mingqianwu98 <smilefish64@gmail.com>
2017-10-21 14:57:14 -0500

    Buggy Event Listener

--- popup.js
+++ popup.js
@@ -8,3 +8,23 @@ $(document).ready(function () {
     });
 });
 
+function getWageValue() {
+    var wage = document.getElementById('wage-input').value;
+}
+
+function getItemValue() {
+    var item = document.getElementById('item-input').value;
+}
+
+function getPriceValue() {
+    var price = document.getElementById('price-input').value;
+}
+
+var wageButton = document.getElementById('inputWageButton');
+wageButton.addEventListener('click', getWageValue, false);
+
+var itemButton = document.getElementById('inputItemButton');
+itemButton.addEventListener('click', getItemValue, false);
+
+var priceButton = document.getElementById('inputPriceButton');
+priceButton.addEventListener('click', getPriceValue, false);
