d71b1e5a5a0883ff6e10c9727229699214640eb2
Sameer <spuri4096@gmail.com>
2017-10-21 17:16:25 -0500

    Real cost amazon updates

--- real-cost-amazon.js
+++ real-cost-amazon.js
@@ -1,24 +1,38 @@
-CURRENCY_SYMBOL="\u0394";
-$(document.ready(function() {
-$(".sx-price").each( function(i, obj) {
-  if($(obj).find('.sx-dash-formatting').length !== 0) {
+CURRENCY_SYMBOL = "\u0394";
+apply = function() {
+  $(".sx-price, .a-price")
+      .each(function(i, obj) {
+        prices = $(obj).find(".sx-price-whole, .a-price-whole");
+        $(obj)
+            .find(".sx-price-fractional, .a-price-fraction")
+            .each(function(id, frac) {
+              prices[id] =
+                  parseFloat($(prices[id]).text() + "." + $(frac).text());
+            });
+        $(obj)
+            .find(".sx-price-whole, .a-price-whole")
+            .each(function(id, whole) {
+              $(whole).text(Math.trunc(prices[id]));
+            });
+        $(obj)
+            .find(".sx-price-fractional, .a-price-fraction")
+            .each(function(id, frac) {
+              $(frac).text(Math.trunc((prices[id] % 1) * 100));
+            });
+      });
+  $(".sx-price-currency, .a-price-symbol").text(CURRENCY_SYMBOL);
 
-  }
-  prices = $(obj).find(".sx-price-whole");
-  $(obj).find(".sx-price-fractional").each(function(id, frac){
-    prices[id] = parseFloat($(prices[id]).text() + "." + $(frac).text());
-  });
-  $(obj).find(".sx-price-whole").each(function(id, whole) { $(whole).text(Math.trunc(prices[id])); });
-  $(obj).find(".sx-price-fractional").each( function(id, frac) { $(frac).text(Math.trunc((prices[id] % 1)*100)); });
-});
-$(".sx-price-currency").text(CURRENCY_SYMBOL);
+  $(".a-color-base,.a-color-price")
+      .each(function(i, obj) {
+        price = $(obj).text();
+        if (!price.startsWith("$")) {
+          return;
+        }
+        price = price.substring(1);
+        price = parseFloat(price);
+        if (price !== 0)
+        $(obj).text(CURRENCY_SYMBOL + price);
+      });
+};
 
-$(".s-price").each(function(i, obj) {
-  price = $(obj).text();
-  alert(price);
-  if (!price.startsWith("$")) { alert('baddie'); return; }
-  price = price.substring(1);
-  $(obj).text(CURRENCY_SYMBOL+price);
-});
-});
-
+$(document).ready(apply);
